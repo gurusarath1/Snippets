e31685f0629510eb220400f493fe90784a7d6709
Guru Sarath <gurusarath1.official@gmail.com>
2020-01-06 04:49:35 -0600

    Regression with NN: comments added with plotting
    Regression with NN: comments added with plotting

--- Python_Learning/Keras/Keras_From_CSV.py
+++ Python_Learning/Keras/Keras_From_CSV.py
@@ -1,10 +1,13 @@
 import numpy as np
 import keras as k
+import matplotlib.pyplot as plt
 
 #function to learn in this csv: OUT=sin(x)+3y+5z
+#Load the dataset
 data = np.loadtxt('DataSet_CSV.csv', dtype=float, delimiter=',', skiprows=1)
 
 
+#Split the dataset into training, validation and test data
 data_X_train = data[:1000,:3]
 data_Y_train = data[:1000,3:]
 
@@ -14,7 +17,7 @@ data_Y_test = data[1000:1500,3:]
 data_X_valid = data[1500:,:3]
 data_Y_valid = data[1500:,3:]
 
-
+#This function generates the model
 def generateModel():
 
 	model = k.models.Sequential()
@@ -33,10 +36,25 @@ def generateModel():
 	return model
 
 
-
+#Generate the model
 m = generateModel()
 
-m.fit(data_X_train, data_Y_train, epochs=300, batch_size=200, validation_data = (data_X_valid, data_Y_valid))
 
+#Fit the dataset
+history = m.fit(data_X_train, data_Y_train, epochs=100, batch_size=200, validation_data = (data_X_valid, data_Y_valid))
+
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+
+#Check the predicted values for some values from the test set
 print(data_X_test[0:10,:])
 print(m.predict(data_X_test[0:10,:]))
+
