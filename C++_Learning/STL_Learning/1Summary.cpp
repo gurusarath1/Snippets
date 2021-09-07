4ace952fd8e429f22ec785b66b998514f725e0c0
Guru Sarath <gurusarath1.official@gmail.com>
2021-09-06 17:10:37 -0700

    Create 1Summary.cpp

---
+++ C++_Learning/STL_Learning/1Summary.cpp
@@ -0,0 +1,22 @@
+/*
+COMMON
+
+size()
+clear()
+
+::iterator
+begin()
+end()
+*/
+
+
+/*
+DATA STRUCTURE - find value of element - add element - remove element
+
+stack - top() - push(x) - pop()
+queue - front() - push(x) - pop()
+priority_queue - top() - push(x) - pop()
+set - ! - insert(x) - erase(x)
+vector - vec[0], vec[vec.size() -1] - push_back(x) - pop_back()
+map - ! - map[x] = y, insert(pair<d1,d2>{x,y})
+*/
