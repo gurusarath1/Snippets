c66f687c65971819c62ff9925280f0fb0db54a67
Guru Sarath <gurusarath1.official@gmail.com>
2022-04-04 23:56:09 -0700

    Update Cpp_PriorityQueue_with_compare.cpp

--- C++_Learning/STL_Learning/Cpp_PriorityQueue_with_compare.cpp
+++ C++_Learning/STL_Learning/Cpp_PriorityQueue_with_compare.cpp
@@ -9,3 +9,22 @@
     
     
     priority_queue<pair<double, int>, vector<pair<double, int>>, Compare> pq;
+
+
+
+
+class Foo
+{
+
+};
+
+bool Compare(Foo, Foo)
+{
+    return true;
+}
+
+int main()
+{
+    std::priority_queue<Foo, std::vector<Foo>, std::function<bool(Foo, Foo)>> pq(Compare);
+    return 0;
+}
